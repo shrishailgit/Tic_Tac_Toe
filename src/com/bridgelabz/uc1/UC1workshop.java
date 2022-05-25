3ea5c796164e5073989d4e53598a8f83650d2a76
shrishail <shrikariagr92@gmail.com>
2022-05-25 08:46:23 +0530

    UC1

---
+++ src/com/bridgelabz/uc1/UC1workshop.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.uc1;
+
+public class UC1workshop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		char[] arr =new char[10];
+		arr[1]=100;
+	}
+
+}
