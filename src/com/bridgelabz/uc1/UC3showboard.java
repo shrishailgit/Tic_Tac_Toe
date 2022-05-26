a653708540c27e2a399be5d4ef5d116b31afa5ab
shrishail <shrikariagr92@gmail.com>
2022-05-26 09:59:05 +0530

    UC3

---
+++ src/com/bridgelabz/uc1/UC3showboard.java
@@ -0,0 +1,31 @@
+package com.bridgelabz.uc1;
+
+public class UC3showboard {
+
+	 
+	 private static char[] dataArray= new char[10];
+	public static void main(String[] args) {
+		
+	
+		// TODO Auto-generated method stub
+
+		
+		UC3showboard.showBoard();
+		
+	}
+		 public static void showBoard() {
+		        System.out.println(" ----------- ");
+		       
+				System.out.println("| " + dataArray[1] + " | " + dataArray[2] + " | " + dataArray[3] + " |");
+		        System.out.println("|-----------|");
+		        System.out.println("| " + dataArray[4] + " | " + dataArray[5] + " | " + dataArray[6] + " |");
+		        System.out.println("|-----------|");
+		        System.out.println("| " + dataArray[7] + " | " + dataArray[8] + " | " + dataArray[9] + " |");
+		        System.out.println(" ----------- ");
+		  
+		        
+		 }
+		     
+		 
+		     
+}
